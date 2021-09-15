babf6b4b4d670a841bd69b7df9a0a0121d51bd18
HNochev <bgaming@abv.bg>
2021-09-15 22:32:52 +0300

    My first commit

---
+++ Programing Basics/Exercise 1 - First Steps In Coding/06.CharityCampaign/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _06.CharityCampaign
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double cakePrice = 45;
+            double wafflePrice = 5.80;
+            double pancakePrice = 3.20;
+
+            int days = int.Parse(Console.ReadLine());
+            int cookers = int.Parse(Console.ReadLine());
+            int cakes = int.Parse(Console.ReadLine());
+            int waffles = int.Parse(Console.ReadLine());
+            int pancakes = int.Parse(Console.ReadLine());
+
+            double fullPriceOfCakes = cakes * cakePrice;
+            double fullPriceOfWaffles = waffles * wafflePrice;
+            double fullPriceOfPancakes = pancakes * pancakePrice;
+            double sumForADay = (fullPriceOfCakes + fullPriceOfPancakes + fullPriceOfWaffles) * cookers;
+            double allDaysSum = sumForADay * days;
+
+            double onlyProfitSum = allDaysSum - (allDaysSum / 8);
+
+            Console.WriteLine($"{onlyProfitSum:0.00}");
+
+        }
+    }
+}
---
+++ Programing Basics/Exercise 1 - First Steps In Coding/08.FishTank/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _08.FishTank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lenght = int.Parse(Console.ReadLine());
+            int width = int.Parse(Console.ReadLine());
+            int height = int.Parse(Console.ReadLine());
+            double notAvailablePercent = double.Parse(Console.ReadLine());
+
+            int volume = lenght * width * height;
+            double liters = volume * 0.001;
+            double finalLiters = liters - (liters * (notAvailablePercent / 100));
+
+            Console.WriteLine(finalLiters);
+        }
+    }
+}
