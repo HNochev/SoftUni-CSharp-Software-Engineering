34d07116ac111a24e858406dc9b9c654e08ddab1
HNochev <bgaming@abv.bg>
2021-09-21 21:53:54 +0300

    Add For Loop Lab

---
+++ Programing Basics/Lab 4 - For Loop/01. NumbersFrom1To100/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace _01._NumbersFrom1To100
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int number = 1; number <= 100; number++)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
---
+++ Programing Basics/Lab 4 - For Loop/02. NumbersN.1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace _02._NumbersN._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; n --)
+            {
+                Console.WriteLine(n);
+            }
+        }
+    }
+}
