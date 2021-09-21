8ccffc861945bc74b16df26ff07f551d3bed0427
HNochev <bgaming@abv.bg>
2021-09-21 21:54:23 +0300

    Add For Loop Lab

---
+++ Programing Basics/Lab 4 - For Loop/06. VowelsSum/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _06._VowelsSum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine().ToLower();
+            double finalSum = 0;
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                char momentLetter = text[i];
+
+                switch(momentLetter)
+                {
+                    case 'a':
+                        {
+                            finalSum = finalSum + 1;
+                            break;
+                        }
+                    case 'e':
+                        {
+                            finalSum = finalSum + 2;
+                            break;
+                        }
+                    case 'i':
+                        {
+                            finalSum = finalSum + 3;
+                            break;
+                        }
+                    case 'o':
+                        {
+                            finalSum = finalSum + 4;
+                            break;
+                        }
+                    case 'u':
+                        {
+                            finalSum = finalSum + 5;
+                            break;
+                        }
+                }
+            }
+            Console.WriteLine(finalSum);
+        }
+    }
+}
---
+++ Programing Basics/Lab 4 - For Loop/07. SumNumbers/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _07._SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int finalSum = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                finalSum = finalSum + number;
+            }
+            Console.WriteLine(finalSum);
+        }
+    }
+}
