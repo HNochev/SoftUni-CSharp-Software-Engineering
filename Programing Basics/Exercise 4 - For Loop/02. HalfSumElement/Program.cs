e4b5dde1ead2ed3f345eb3c78585c032a7324ba0
HNochev <bgaming@abv.bg>
2021-09-22 16:23:46 +0300

    Add For Loop Exercise

---
+++ Programing Basics/Exercise 4 - For Loop/02. HalfSumElement/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _02._HalfSumElement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int max = int.MinValue;
+            int finalSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (number > max)
+                {
+                    max = number;
+                }
+                finalSum = finalSum + number;
+            }
+            finalSum = finalSum - max;
+            if(finalSum == max)
+            {
+                Console.WriteLine("Yes");
+                Console.WriteLine($"Sum = {finalSum}");
+            }
+            else
+            {
+                Console.WriteLine("No");
+                Console.WriteLine($"Diff = {Math.Abs(finalSum-max)}");
+            }
+        }
+    }
+}
---
+++ Programing Basics/Exercise 4 - For Loop/03. OddEvenPosition/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Transactions;
+
+namespace _03._OddEvenPosition
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double evenMax = double.MinValue;
+            double oddMax = double.MinValue;
+            double evenMin = double.MaxValue;
+            double oddMin = double.MaxValue;
+            double evenSum = 0;
+            double oddSum = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                double number = double.Parse(Console.ReadLine());
+                if(i%2==0)
+                {
+                    evenSum = evenSum + number;
+                    if(number > evenMax)
+                    {
+                        evenMax = number;
+                    }
+                    if(number < evenMin)
+                    {
+                        evenMin = number;
+                    }
+                }
+                else
+                {
+                    oddSum = oddSum + number;
+                    if (number > oddMax)
+                    {
+                        oddMax = number;
+                    }
+                    if (number < oddMin)
+                    {
+                        oddMin = number;
+                    }
+                }
+            }
+            if (oddMin != double.MaxValue && oddMax != double.MinValue && evenMin != double.MaxValue && evenMax != double.MinValue) 
+            {
+                Console.WriteLine($"OddSum={oddSum:f2},");
+                Console.WriteLine($"OddMin={oddMin:f2},");
+                Console.WriteLine($"OddMax={oddMax:f2},");
+                Console.WriteLine($"EvenSum={evenSum:f2},");
+                Console.WriteLine($"EvenMin={evenMin:f2},");
+                Console.WriteLine($"EvenMax={evenMax:f2}");
+            }
+            else if(oddMin == double.MaxValue && oddMax == double.MinValue && evenMax == double.MinValue && evenMin == double.MaxValue)
+            {
+                Console.WriteLine($"OddSum={oddSum:f2},");
+                Console.WriteLine($"OddMin=No,");
+                Console.WriteLine($"OddMax=No,");
+                Console.WriteLine($"EvenSum={evenSum:f2},");
+                Console.WriteLine($"EvenMin=No,");
+                Console.WriteLine($"EvenMax=No");
+            }
+            else if (oddMin != double.MaxValue && oddMax != double.MinValue && evenMax == double.MinValue && evenMin == double.MaxValue)
+            {
+                Console.WriteLine($"OddSum={oddSum:f2},");
+                Console.WriteLine($"OddMin={oddMin:f2},");
+                Console.WriteLine($"OddMax={oddMax:f2},");
+                Console.WriteLine($"EvenSum={evenSum:f2},");
+                Console.WriteLine($"EvenMin=No,");
+                Console.WriteLine($"EvenMax=No");
+            }
+        }
+    }
+}
