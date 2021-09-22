7478b1a37f914b61aa677168c2671c90677b8190
HNochev <bgaming@abv.bg>
2021-09-22 16:24:32 +0300

    Add For Loop Exercise

---
+++ Programing Basics/Exercise 4 - For Loop/06. Salary/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Net.Sockets;
+
+namespace _06._Salary
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int tabs = int.Parse(Console.ReadLine());
+            double salary = double.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= tabs; i++)
+            {
+                string site = Console.ReadLine();
+                if (site == "Facebook")
+                {
+                    salary = salary - 150;
+                }
+                else if (site == "Instagram")
+                {
+                    salary = salary - 100;
+                }
+                else if (site == "Reddit")
+                {
+                    salary = salary - 50;
+                }
+                if (salary <= 0)
+                {
+                    Console.WriteLine("You have lost your salary.");
+                    break;
+                }
+            }
+            if (salary > 0)
+            {
+                Console.WriteLine(salary);
+            }
+        }
+    }
+}
