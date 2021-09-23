ceeff3ebd8469d0f0a796353827d445469df70da
HNochev <bgaming@abv.bg>
2021-09-23 13:43:34 +0300

    Add While Loop Lab

---
+++ Programing Basics/Lab 5 - While Loop/08. GraduationPt.2/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace _08._GraduationPt._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = Console.ReadLine();
+            double finalScore = 0;
+            int tries = 0;
+            int i = 0;
+
+            for (i = 0; i < 12; i++)
+            {
+                double score = double.Parse(Console.ReadLine());
+                if(score < 4)
+                {
+                    tries++;
+                    score = 0;
+                    if(tries == 2)
+                    {
+                        break;
+                    }
+                }
+                finalScore = finalScore + score;
+            }
+            if (i == 12)
+            {
+                finalScore = finalScore / i;
+                Console.WriteLine($"{name} graduated. Average grade: {finalScore:f2}");
+            }
+            else
+            {
+                Console.WriteLine($"{name} has been excluded at {i} grade");
+            }
+        }
+    }
+}
