354800629bc344ce806f165f13ebee9822a72775
HNochev <bgaming@abv.bg>
2021-09-23 13:42:42 +0300

    Add While Loop Lab

---
+++ Programing Basics/Lab 5 - While Loop/03. SumNumbers/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _03._SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int max = int.Parse(Console.ReadLine());
+            int sum = 0;
+
+            while (max > sum)
+            {
+                int number = int.Parse(Console.ReadLine());
+                sum = sum + number;
+            }
+            Console.WriteLine(sum);
+            
+        }
+    }
+}
