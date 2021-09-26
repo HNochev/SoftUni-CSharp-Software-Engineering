556252ba626de7106e030f4c450d0924c388ea26
HNochev <bgaming@abv.bg>
2021-09-26 18:20:42 +0300

    Add Nested Loops Exercise

---
+++ Programing Basics/Exercise 6 - Nested Loops/04. TrainTheTrainers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace _04._TrainTheTrainers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int countJudges = int.Parse(Console.ReadLine());
+            string text = Console.ReadLine();
+            double finalMark = 0;
+            double finalFinalMark = 0;
+            int allMarks = 0;
+
+            while (text != "Finish")
+            {
+                for (int i = 0; i < countJudges; i++)
+                {
+                    double mark = double.Parse(Console.ReadLine());
+                    finalMark = finalMark + mark;
+                    allMarks++;
+                }
+                Console.WriteLine($"{text} - {finalMark/countJudges:f2}.");
+                finalFinalMark = finalFinalMark + finalMark;
+                finalMark = 0;
+                text = Console.ReadLine();
+            }
+            Console.WriteLine($"Student's final assessment is {finalFinalMark/allMarks:f2}.");
+        }
+    }
+}
---
+++ Programing Basics/Exercise 6 - Nested Loops/06. SpecialNumbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _06._SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int number = 1111; number <= 9999; number++)
+            {
+                int tousands = number / 1000;
+                int hundreds = (number / 100) % 10;
+                int tens = (number / 10) % 10;
+                int units = number % 10;
+
+                bool special = tousands != 0 && hundreds != 0 && tens != 0 && units != 0 && n % tousands == 0 && n % hundreds == 0 && n % tens == 0 && n % units == 0;
+
+                if (special)
+                {
+                    Console.Write($"{number} ");
+                }
+            }
+        }
+    }
+}
