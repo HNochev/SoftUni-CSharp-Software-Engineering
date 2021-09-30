43898e218fe77b0d1f0186ffe906bff8713fa294
HNochev <bgaming@abv.bg>
2021-09-30 18:53:51 +0300

    Add More Exercises

---
+++ Programing Basics/More Exercises - Bonus/4. VB While-Loop - More Exercises/01. Dishwasher/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace _01._Dishwasher
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int detergentBottles = int.Parse(Console.ReadLine());
+            double detergentQuantity = detergentBottles * 750;
+            double detergentUsed = 0;
+            string dishes = "0";
+            double allDishes = 0;
+            double allPots = 0;
+            double counter = 0;
+            while (dishes != "End")
+            {
+                int dishesInt = int.Parse(dishes);
+                if (counter % 3 == 0)
+                {
+                    counter = 0;
+                    allPots = allPots + dishesInt;
+                    detergentUsed = dishesInt * 15;
+                    if (detergentQuantity < detergentUsed)
+                    {
+                        break;
+                    }
+                    detergentQuantity = detergentQuantity - detergentUsed;
+                }
+                else
+                {
+                    allDishes = allDishes + dishesInt;
+                    detergentUsed = dishesInt * 5;
+                    if (detergentQuantity <= detergentUsed)
+                    {
+                        break;
+                    }
+                    detergentQuantity = detergentQuantity - detergentUsed;
+                }
+                counter++;
+                dishes = Console.ReadLine();
+            }
+            if (dishes == "End")
+            {
+                Console.WriteLine("Detergent was enough!");
+                Console.WriteLine($"{allDishes} dishes and {allPots} pots were washed.");
+                Console.WriteLine($"Leftover detergent {detergentQuantity} ml.");
+            }
+            else if (detergentQuantity <= detergentUsed)
+            {
+                Console.WriteLine($"Not enough detergent, {Math.Abs(detergentQuantity - detergentUsed)} ml. more necessary!");
+            }
+        }
+    }
+}
---
+++ Programing Basics/More Exercises - Bonus/4. VB While-Loop - More Exercises/02. ReportSystem/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+
+namespace _02._ReportSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int expectedSum = int.Parse(Console.ReadLine());
+            string money = Console.ReadLine();
+            int counter = 0;
+            double averageCard = 0;
+            double averageCash = 0;
+            int cardCounter = 0;
+            int cashCounter = 0;
+            double finalSum = 0;
+
+            while (money != "End")
+            {
+                double moneyInt = double.Parse(money);
+                if (counter % 2 == 0)
+                {
+                    if (moneyInt > 100 && moneyInt != 0)
+                    {
+                        Console.WriteLine("Error in transaction! ");
+                    }
+                    else
+                    {
+                        cashCounter++;
+                        averageCash = averageCash + moneyInt;
+                        Console.WriteLine("Product sold!");
+                        finalSum = finalSum + moneyInt;
+                    }
+                    if (finalSum >= expectedSum)
+                    {
+                        Console.WriteLine($"Average CS: {averageCash / cashCounter:f2}");
+                        Console.WriteLine($"Average CC: {averageCard / cardCounter:f2}");
+                        break;
+                    }
+                }
+                else
+                {
+                    if (moneyInt < 10 && moneyInt != 0)
+                    {
+                        Console.WriteLine("Error in transaction! ");
+                    }
+                    else
+                    {
+                        cardCounter++;
+                        averageCard = averageCard + moneyInt;
+                        Console.WriteLine("Product sold!");
+                        finalSum = finalSum + moneyInt;
+                    }
+                }
+                if (finalSum >= expectedSum)
+                {
+                    Console.WriteLine($"Average CS: {averageCash / cashCounter:f2}");
+                    Console.WriteLine($"Average CC: {averageCard / cardCounter:f2}");
+                    break;
+                }
+                counter++;
+                money = Console.ReadLine();
+            }
+            if(money == "End")
+            {
+                Console.WriteLine("Failed to collect required money for charity.");
+            }
+        }
+    }
+}
