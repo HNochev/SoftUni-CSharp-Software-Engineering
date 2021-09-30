638cabfc40d151f3e3ea11e68193782367f5e9f5
HNochev <bgaming@abv.bg>
2021-09-30 18:54:58 +0300

    Add More Exercises

---
+++ Programing Basics/More Exercises - Bonus/4. VB While-Loop - More Exercises/05. AverageNumber/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05._AverageNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            double finalSum = 0;
+            int counter = 0;
+
+            for (int i = 0; i < n; i++)
+            {
+                double sum = double.Parse(Console.ReadLine());
+                finalSum = finalSum + sum;
+                counter++;
+            }
+            Console.WriteLine($"{finalSum / counter:f2}");
+        }
+    }
+}
