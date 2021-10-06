66f8f86dc4b7f3d1d30e2cd7e3a9f70705aa49b1
HNochev <bgaming@abv.bg>
2021-10-06 21:25:32 +0300

    Add Basics Revision Lab

---
+++ Programming Fundamentals/Lab 1 - Basic Syntax, Conditional Statements and Loops/04.BackIn30Minutes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _04.BackIn30Minutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hours = int.Parse(Console.ReadLine());
+            int minutes = int.Parse(Console.ReadLine());
+
+            minutes = minutes + 30;
+
+            if(minutes >= 60)
+            {
+                hours++;
+                minutes = minutes - 60;
+            }
+            if(hours > 23)
+            {
+                hours = hours - 24;
+            }
+            Console.WriteLine($"{hours}:{minutes:d2}");
+        }
+    }
+}
