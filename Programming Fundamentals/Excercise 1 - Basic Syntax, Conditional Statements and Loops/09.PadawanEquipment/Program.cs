1bc2825824814031b05d8ddc09bcacfd6cce59bd
HNochev <bgaming@abv.bg>
2021-10-07 19:41:04 +0300

    Add Basics Revision Exercise

---
+++ Programming Fundamentals/Excercise 1 - Basic Syntax, Conditional Statements and Loops/09.PadawanEquipment/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace _09.PadawanEquipment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double money = double.Parse(Console.ReadLine());
+            int students = int.Parse(Console.ReadLine());
+            double lightsaberPrice = double.Parse(Console.ReadLine());
+            double robesPrice = double.Parse(Console.ReadLine());
+            double beltsPrice = double.Parse(Console.ReadLine());
+
+            double freeBelts = 0;
+            for (int i = 1; i <= students; i++)
+            {
+                if (i % 6 == 0)
+                {
+                    freeBelts++;
+                }
+            }
+
+            double fullLightsabers = students * 1.10;
+            fullLightsabers = Math.Ceiling(fullLightsabers);
+
+            double finalPrice = (students * robesPrice) + ((students - freeBelts) * beltsPrice) + (fullLightsabers * lightsaberPrice);
+
+
+
+            if (money >= finalPrice)
+            {
+                Console.WriteLine($"The money is enough - it would cost {finalPrice:f2}lv.");
+            }
+            else
+            {
+                Console.WriteLine($"Ivan Cho will need {finalPrice - money:f2}lv more.");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Excercise 1 - Basic Syntax, Conditional Statements and Loops/10.RageExpenses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10.RageExpenses
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGames = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int headsets = lostGames / 2;
+            int mouses = lostGames / 3;
+            int keyboards = lostGames / 6;
+            int displays = lostGames / 12;
+
+            double finalPrice = (headsetPrice * headsets) + (mousePrice * mouses) + (keyboardPrice * keyboards) + (displayPrice * displays);
+
+            Console.WriteLine($"Rage expenses: {finalPrice:f2} lv.");
+        }
+    }
+}
