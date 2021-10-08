040ae615f66baf788ab6a01b41e156937e7d772d
HNochev <bgaming@abv.bg>
2021-10-08 14:23:42 +0300

    Add Data Types and Variables Lab

---
+++ Programming Fundamentals/Lab 2 - Data Types and Variables/01.ConvertMetersToKilometers/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _01.ConvertMetersToKilometers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            decimal meters = decimal.Parse(Console.ReadLine());
+            decimal km = meters / 1000.0M;
+            Console.WriteLine($"{km:f2}");
+        }
+    }
+}
---
+++ Programming Fundamentals/Lab 2 - Data Types and Variables/04.CenturiesToMinutes/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _04.CenturiesToMinutes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint centuries = uint.Parse(Console.ReadLine());
+            ulong years = centuries * 100;
+            decimal days = years * 365.2422M;
+            decimal hours = Math.Floor(days) * 24.0M;
+            decimal minutes = hours * 60.0M;
+
+            Console.WriteLine($"{centuries} centuries = {years} years = {Math.Floor(days)} days = {hours:f0} hours = {minutes:f0} minutes");
+        }
+    }
+}
