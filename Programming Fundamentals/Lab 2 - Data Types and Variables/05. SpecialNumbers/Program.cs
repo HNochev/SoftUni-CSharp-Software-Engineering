2eec3e8d5837eb3b270ba56cbf8b5fabf2f519fe
HNochev <bgaming@abv.bg>
2021-10-08 14:25:32 +0300

    Add Data Types and Variables Lab

---
+++ Programming Fundamentals/Lab 2 - Data Types and Variables/05. SpecialNumbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _05._SpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            for (int i = 1; i <= number; i++)
+            {
+                int sumOfDigits = 0;
+                int oneDigit = i;
+                while (oneDigit > 0)
+                {
+                    sumOfDigits = sumOfDigits + (oneDigit % 10);
+                    oneDigit = oneDigit / 10;
+                }
+                bool isSpecial = (sumOfDigits == 5) || (sumOfDigits == 7) || (sumOfDigits == 11);
+                Console.WriteLine($"{i} -> {isSpecial}");
+            }
+        }
+    }
+}
