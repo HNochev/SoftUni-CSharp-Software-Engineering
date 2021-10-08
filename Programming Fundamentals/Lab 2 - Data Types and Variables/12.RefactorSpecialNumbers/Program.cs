2f87c511f58da68d308bb4c12ae7ebed97dd3705
HNochev <bgaming@abv.bg>
2021-10-08 14:28:51 +0300

    Add Data Types and Variables Lab

---
+++ Programming Fundamentals/Lab 2 - Data Types and Variables/12.RefactorSpecialNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace _12.RefactorSpecialNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sumOfDigit = 0;
+            int digit = 0;
+
+            for (int i = 1; i <= n; i++)
+            {
+                digit = i;
+                while (digit > 0)
+                {
+                    sumOfDigit += digit % 10;
+                    digit = digit / 10;
+                }
+                bool isSpecial = (sumOfDigit == 5) || (sumOfDigit == 7) || (sumOfDigit == 11);
+                Console.WriteLine($"{i} -> {isSpecial}");
+                sumOfDigit = 0;
+            }
+
+        }
+    }
+}
