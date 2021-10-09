7af0a8a51b136e2dc0c42d886a566ef2a20a0294
HNochev <bgaming@abv.bg>
2021-10-09 22:55:38 +0300

    Add Data Types and Variables Exercise

---
+++ Programming Fundamentals/Exercise 2 - Data Types and Variables/09.SpiceMustFlow/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09.SpiceMustFlow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int startingYield = int.Parse(Console.ReadLine());
+            int days = 0;
+            int sum = 0;
+
+            while(startingYield >= 100)
+            {
+                sum = sum + (startingYield-26);
+                startingYield = startingYield - 10;
+                if(startingYield<100)
+                {
+                    sum = sum - 26;
+                }
+                days++;
+            }
+            Console.WriteLine(days);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ Programming Fundamentals/Exercise 2 - Data Types and Variables/10.PokeMon/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _10.PokeMon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int m = int.Parse(Console.ReadLine());
+            int y = int.Parse(Console.ReadLine());
+            int targets = 0;
+            int originalN = n;
+
+            while(n>=m)
+            {
+                if (n == originalN / 2 && y > 0)
+                {
+                    n = n / y;
+                    if(n<m)
+                    {
+                        break;
+                    }
+                }
+
+                n = n - m;
+                targets++;
+            }
+            Console.WriteLine(n);
+            Console.WriteLine(targets);
+        }
+    }
+}
