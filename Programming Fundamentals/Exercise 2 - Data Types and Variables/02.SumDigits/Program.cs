b875fddf4ee793ed242ee824dff3007c3b10b7df
HNochev <bgaming@abv.bg>
2021-10-09 22:52:34 +0300

    Add Data Types and Variables Exercise

---
+++ Programming Fundamentals/Exercise 2 - Data Types and Variables/02.SumDigits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _02.SumDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string number = Console.ReadLine();
+            int sum = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                sum = sum + int.Parse(number[i].ToString());
+            }
+
+            Console.WriteLine(sum);
+        }
+    }
+}
