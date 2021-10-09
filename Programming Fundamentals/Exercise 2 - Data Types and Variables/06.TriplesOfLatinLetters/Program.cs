f908ca7898ba4e39cab0aef8e70ee104a301eea9
HNochev <bgaming@abv.bg>
2021-10-09 22:54:07 +0300

    Add Data Types and Variables Exercise

---
+++ Programming Fundamentals/Exercise 2 - Data Types and Variables/06.TriplesOfLatinLetters/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _06.TriplesOfLatinLetters
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine())+'a';
+            
+
+            for (char i = 'a'; i < n; i++)
+            {
+                for (char j = 'a'; j < n; j++)
+                {
+                    for (char k = 'a'; k < n; k++)
+                    {
+                        Console.WriteLine($"{i}{j}{k}");
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Exercise 2 - Data Types and Variables/07. WaterOverflow/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._WaterOverflow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte n = byte.Parse(Console.ReadLine());
+            ushort sum = 0;
+
+            for (byte i = 0; i < n; i++)
+            {
+                ushort liters = ushort.Parse(Console.ReadLine());
+
+                if(sum + liters > 255)
+                {
+                    Console.WriteLine("Insufficient capacity!");
+                    continue;
+                }
+                sum = (ushort)(sum + liters);
+            }
+            Console.WriteLine($"{sum}");
+        }
+    }
+}
