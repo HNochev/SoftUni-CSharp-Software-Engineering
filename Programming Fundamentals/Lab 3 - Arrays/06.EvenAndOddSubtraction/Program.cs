1f848c756e719b0e426957ed92782d54cf92b59d
HNochev <bgaming@abv.bg>
2021-10-10 20:16:15 +0300

    Add Arrays Lab

---
+++ Programming Fundamentals/Lab 3 - Arrays/06.EvenAndOddSubtraction/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+
+namespace _06.EvenAndOddSubtraction
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] number = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int evenSum = 0;
+            int oddSum = 0;
+            int evenOddSubtraction = 0;
+
+            for (int i = 0; i < number.Length; i++)
+            {
+                if(number[i]%2==0)
+                {
+                    evenSum = evenSum + number[i];
+                }
+                else
+                {
+                    oddSum = oddSum + number[i];
+                }
+            }
+            evenOddSubtraction = evenSum - oddSum;
+
+
+            Console.WriteLine($"{evenOddSubtraction}");
+        }
+    }
+}
