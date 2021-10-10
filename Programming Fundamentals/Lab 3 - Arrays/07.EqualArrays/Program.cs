d5d385b48e034c1555df4ba000095156481b3154
HNochev <bgaming@abv.bg>
2021-10-10 20:18:12 +0300

    Add Arrays Lab

---
+++ Programming Fundamentals/Lab 3 - Arrays/07.EqualArrays/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.EqualArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] first = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+            int[] second = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int sum = 0;
+            int counter = 0;
+
+            for (int i = 0; i < first.Length; i++)
+            {
+                if (first[i] == second[i])
+                {
+                    sum = sum + first[i];
+                }
+                else
+                {
+                    Console.WriteLine($"Arrays are not identical. Found difference at {i} index");
+                    break;
+                }
+                counter++;
+            }
+            if (counter == first.Length)
+            {
+                Console.WriteLine($"Arrays are identical. Sum: {sum}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Lab 3 - Arrays/08.CondenseArrayToNumber/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.CondenseArrayToNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split()
+                .Select(int.Parse)
+                .ToArray();
+
+            int totalElements = numbers.Count();
+            int[] condensed = new int[totalElements-1];
+            while (totalElements != 1)
+            {
+                for (int i = 0; i < numbers.Length-1; i++)
+                {
+                    condensed[i] = numbers[i] + numbers[i + 1];
+                }
+
+                for (int i = 0; i < condensed.Length; i++)
+                {
+                    numbers[i] = condensed[i];
+                }
+                totalElements--;
+            }
+            Console.WriteLine($"{numbers[0]}");
+        }
+    }
+}
