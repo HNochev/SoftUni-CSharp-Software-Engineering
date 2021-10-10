d26e5f318d2365c49b98fe9b49fe23a7c18d57c1
HNochev <bgaming@abv.bg>
2021-10-10 20:14:52 +0300

    Add Arrays Lab

---
+++ Programming Fundamentals/Lab 3 - Arrays/01.DayOfWeek/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace _01.DayOfWeek
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int day = int.Parse(Console.ReadLine());
+            string[] weekDay = new string[] { "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday" };
+            if (day >= 1 && day <= 7)
+            {
+                Console.WriteLine($"{weekDay[day - 1]}");
+            }
+            else
+            {
+                Console.WriteLine("Invalid day!");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Lab 3 - Arrays/02. PrintNumbersInReverseOrder/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _02._PrintNumbersInReverseOrder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] arr = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                arr[i] = int.Parse(Console.ReadLine());
+            }
+
+            int[] newArr = arr.Reverse().ToArray();
+
+            Console.WriteLine(string.Join(' ', arr.Reverse()));
+        }
+    }
+}
