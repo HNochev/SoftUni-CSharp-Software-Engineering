d84f39737deb515a6aa7fb15e9edcfab1603fad9
HNochev <bgaming@abv.bg>
2021-10-11 20:37:47 +0300

    Add Arrays Exercise

---
+++ Programming Fundamentals/Exercise 3 - Arrays/09.KaminoFactory/Program.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Linq;
+
+namespace _09.KaminoFactory
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int DNALength = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+
+            int bestLenght = 1;
+            int bestStartIndex = 0;
+            int bestSequenceSum = 0;
+            int sequenceCounter = 0;
+            int bestSequenceConter = 1;
+            int[] bestArray = new int[DNALength];
+
+
+            while (input != "Clone them!")
+            {
+                int[] array = input
+                    .Split('!', StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                sequenceCounter++;
+
+                int lenght = 1;
+                int bestCurrentLenght = 1;
+                int start = 0;
+                int currentSequenceSum = 0;
+
+                for (int i = 0; i < array.Length - 1; i++)
+                {
+                    if (array[i] == array[i + 1])
+                    {
+                        lenght++;
+                    }
+                    else
+                    {
+                        lenght = 1;
+                    }
+
+                    if (lenght > bestCurrentLenght)
+                    {
+                        bestCurrentLenght = lenght;
+                        start = i;
+                    }
+                    currentSequenceSum = currentSequenceSum + array[i];
+                }
+                currentSequenceSum = currentSequenceSum + array[DNALength-1];
+
+                if (bestCurrentLenght > bestLenght)
+                {
+                    bestLenght = bestCurrentLenght;
+                    bestStartIndex = start;
+                    bestSequenceSum = currentSequenceSum;
+                    bestSequenceConter = sequenceCounter;
+                    bestArray = array.ToArray();
+                }
+                else if (bestCurrentLenght == bestLenght)
+                {
+                    if (start < bestStartIndex)
+                    {
+                        bestLenght = bestCurrentLenght;
+                        bestStartIndex = start;
+                        bestSequenceSum = currentSequenceSum;
+                        bestSequenceConter = sequenceCounter;
+                        bestArray = array.ToArray();
+                    }
+                    else if (start == bestStartIndex)
+                    {
+                        if (currentSequenceSum > bestSequenceSum)
+                        {
+                            bestLenght = bestCurrentLenght;
+                            bestStartIndex = start;
+                            bestSequenceSum = currentSequenceSum;
+                            bestSequenceConter = sequenceCounter;
+                            bestArray = array.ToArray();
+                        }
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine($"Best DNA sample {bestSequenceConter} with sum: {bestSequenceSum}.");
+            Console.WriteLine(String.Join(' ', bestArray));
+        }
+    }
+}
---
+++ Programming Fundamentals/Exercise 3 - Arrays/10.LadyBugs/Program.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.LadyBugs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int sizeOfField = int.Parse(Console.ReadLine());
+
+            int[] indexesOfLaybugs = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int[] field = new int[sizeOfField];
+
+            for (int i = 0; i < sizeOfField; i++)
+            {
+                for (int j = 0; j < indexesOfLaybugs.Length; j++)
+                {
+                    if (i == indexesOfLaybugs[j])
+                    {
+                        field[i] = 1;
+                    }
+                }
+            }
+
+            string input = Console.ReadLine();
+            while (input != "end")
+            {
+                string[] stringArray = input.Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                int ladybugIndex = int.Parse(stringArray[0]);
+                string direction = stringArray[1];
+                int flyLenght = int.Parse(stringArray[2]);
+
+                if (ladybugIndex < 0 || ladybugIndex > field.Length - 1 || field[ladybugIndex] == 0)
+                {
+                    input = Console.ReadLine();
+                    continue;
+                }
+
+                field[ladybugIndex] = 0;
+
+                if (direction == "right")
+                {
+                    int landIndex = ladybugIndex + flyLenght;
+
+                    if (landIndex > field.Length - 1)
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+
+                    if (field[landIndex] == 1)
+                    {
+                        while (field[landIndex] == 1)
+                        {
+                            landIndex = landIndex + flyLenght;
+                            if (landIndex > field.Length - 1)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (landIndex >= 0 && landIndex <= field.Length - 1)
+                    {
+                        field[landIndex] = 1;
+                    }
+                }
+                else if (direction == "left")
+                {
+                    int landIndex = ladybugIndex - flyLenght;
+
+                    if (landIndex < 0)
+                    {
+                        input = Console.ReadLine();
+                        continue;
+                    }
+
+                    if (field[landIndex] == 1)
+                    {
+                        while (field[landIndex] == 1)
+                        {
+                            landIndex = landIndex - flyLenght;
+                            if (landIndex < 0)
+                            {
+                                break;
+                            }
+                        }
+                    }
+                    if (landIndex >= 0 && landIndex <= field.Length - 1)
+                    {
+                        field[landIndex] = 1;
+                    }
+                }
+
+                input = Console.ReadLine();
+            }
+            Console.WriteLine(String.Join(' ', field));
+        }
+    }
+}
