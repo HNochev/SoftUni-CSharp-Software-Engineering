1db664e010cae763edc3e9dd5f1e545b1b79fc8d
HNochev <bgaming@abv.bg>
2021-10-11 20:37:56 +0300

    Add Arrays Exercise

---
+++ Programming Fundamentals/Exercise 3 - Arrays/07.MaxSequenceOfEqualElements/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.MaxSequenceOfEqualElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int stringLong = 1;
+            int longestString = 0;
+            int start = 0;
+            int startOfLongestString = 0;
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (array[i - 1] == array[i])
+                {
+                    stringLong++;
+                }
+                else
+                {
+                    stringLong = 1;
+                    start = i;
+                }
+
+                if (stringLong > longestString)
+                {
+                    longestString = stringLong;
+                    startOfLongestString = start;
+                }
+            }
+            for (int i = startOfLongestString; i < startOfLongestString+longestString; i++)
+            {
+                Console.Write($"{array[i]} ");
+            }
+        }
+    }
+}
