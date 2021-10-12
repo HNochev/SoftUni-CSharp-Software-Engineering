a56c129fae74f402bb010bf5c9ad34affeff579f
HNochev <bgaming@abv.bg>
2021-10-12 18:58:23 +0300

    Add Method Lab

---
+++ Programming Fundamentals/Lab 4 - Methods/10.MultiplyEvensByOdds/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Linq;
+
+namespace _10.MultiplyEvensByOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int number = Math.Abs(int.Parse(Console.ReadLine()));
+
+            Console.WriteLine(GetMultiplyEvenANdOdds(number));
+
+        }
+        static int GetMultiplyEvenANdOdds(int num)
+        {
+            int result = SumOfEvenNums(num) * SumOfOddNums(num);
+            return result;
+        }
+        static int SumOfEvenNums(int num)
+        {
+            int symbol = 0;
+            int evenSum = 0;
+
+            while(num != 0)
+            {
+                symbol = num % 10;
+                if (symbol % 2 == 0)
+                {
+                    evenSum = evenSum + symbol;
+                }
+                num = num / 10;
+            }
+            return evenSum;
+        }
+        static int SumOfOddNums(int num)
+        {
+            int symbol = 0;
+            int oddSum = 0;
+
+            while (num != 0)
+            {
+                symbol = num % 10;
+                if (symbol % 2 != 0)
+                {
+                    oddSum = oddSum + symbol;
+                }
+                num = num / 10;
+            }
+            return oddSum;
+        }
+    }
+}
---
+++ Programming Fundamentals/Lab 4 - Methods/11.MathOperations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _11.MathOperations
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double first = double.Parse(Console.ReadLine());
+            string symbol = Console.ReadLine();
+            double second = double.Parse(Console.ReadLine());
+
+            Console.WriteLine(Calculate(first, second, symbol));
+        }
+
+        static double Calculate(double a, double b, string action)
+        {
+            double result = 0;
+            switch (action)
+            {
+                case "/":
+                    {
+                        result = a / b;
+                        break;
+                    }
+                case "*":
+                    {
+                        result = a * b;
+                        break;
+                    }
+                case "+":
+                    {
+                        result = a + b;
+                        break;
+                    }
+                case "-":
+                    {
+                        result = a - b;
+                        break;
+                    }
+            }
+            return result;
+        }
+    }
+}
