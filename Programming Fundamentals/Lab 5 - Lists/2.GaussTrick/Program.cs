7f81a85a4c55af800eb88d39b977d2617da14ee2
HNochev <bgaming@abv.bg>
2021-10-14 20:23:41 +0300

    Add Lists Lab

---
+++ Programming Fundamentals/Lab 5 - Lists/2.GaussTrick/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _2.GaussTrick
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> gausNumbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int halfListCount = gausNumbers.Count / 2;
+
+            for (int i = 0; i < halfListCount; i++)
+            {
+                gausNumbers[i] = gausNumbers[i] + gausNumbers[gausNumbers.Count - 1];
+                gausNumbers.RemoveAt(gausNumbers.Count - 1);
+            }
+
+            Console.WriteLine(string.Join(' ', gausNumbers));
+        }
+    }
+}
---
+++ Programming Fundamentals/Lab 5 - Lists/3.MergingLists/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _3.MergingLists
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> firstList = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> secondList = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            List<int> result = new List<int>();
+
+            for (int i = 0; i < Math.Min(firstList.Count, secondList.Count); i++)
+            {
+                result.Add(firstList[i]);
+                result.Add(secondList[i]);
+            }
+
+            for (int i = Math.Min(firstList.Count, secondList.Count); i < Math.Max(firstList.Count, secondList.Count); i++)
+            {
+                if (firstList.Count > secondList.Count)
+                {
+                    result.Add(firstList[i]);
+                }
+                else
+                {
+                    result.Add(secondList[i]);
+                }
+            }
+
+            Console.WriteLine(string.Join(' ', result));
+        }
+    }
+}
