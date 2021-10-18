efa39cd8b7867814708f61296a451e5333613530
HNochev <bgaming@abv.bg>
2021-10-18 23:21:26 +0300

    Add Associative Arrays Lab

---
+++ Programming Fundamentals/Lab 7 - Associative Arrays/04.Largest3Numbers/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.Largest3Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            nums.Sort();
+            nums.Reverse();
+
+            if (nums.Count > 3)
+            {
+                for (int i = 0; i < 3; i++)
+                {
+                    Console.Write($"{nums[i]} ");
+                }
+            }
+            else
+            {
+                Console.WriteLine(string.Join(' ', nums));
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Lab 7 - Associative Arrays/05.WordFilter/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Linq;
+
+namespace _05.WordFilter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Where(x => x.Length % 2 == 0)
+                .ToList()
+                .ForEach(x => Console.WriteLine(x));
+        }
+    }
+}
