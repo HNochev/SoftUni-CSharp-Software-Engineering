1d647487a4bc4cdfab35bd51079034e9fc8e35eb
HNochev <bgaming@abv.bg>
2021-10-18 23:19:31 +0300

    Add Associative Arrays Lab

---
+++ Programming Fundamentals/Lab 7 - Associative Arrays/01. Count Real Numbers/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
+
+namespace _01._Count_Real_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            SortedDictionary<double, int> count = new SortedDictionary<double, int>();
+
+            foreach (var item in numbers)
+            {
+                if(count.ContainsKey(item))
+                {
+                    count[item]++;
+                }
+                else
+                {
+                    count.Add(item, 1);
+                }
+            }
+            foreach (var number in count)
+            {
+                Console.WriteLine($"{number.Key} -> {number.Value}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Lab 7 - Associative Arrays/02.OddOccurrences/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.OddOccurrences
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] text = Console.ReadLine()
+                .ToLower()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Dictionary<string, int> count = new Dictionary<string, int>();
+
+            foreach (var item in text)
+            {
+                if (count.ContainsKey(item))
+                {
+                    count[item]++;
+                }
+                else
+                {
+                    count.Add(item, 1);
+                }
+            }
+            foreach (var item in count)
+            {
+                if (item.Value % 2 != 0)
+                {
+                    Console.Write($"{item.Key} ");
+                }
+            }
+        }
+    }
+}
