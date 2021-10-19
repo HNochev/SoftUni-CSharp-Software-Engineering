cb278e58c37beafa6a16d5f72f0194123550fca3
HNochev <bgaming@abv.bg>
2021-10-19 20:13:24 +0300

    Add Associative Arrays Exercise

---
+++ Programming Fundamentals/Exercise 7 - Associative Arrays/04.Orders/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _04.Orders
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            Dictionary<string, List<double>> products = new Dictionary<string, List<double>>();
+
+            while (input != "buy")
+            {
+                string[] data = input
+                    .Split(' ', StringSplitOptions.RemoveEmptyEntries);
+
+                if (!products.ContainsKey(data[0]))
+                {
+                    products.Add(data[0], new List<double> { double.Parse(data[1]), double.Parse(data[2]) });
+                }
+                else
+                {
+                    products[data[0]][1] = products[data[0]][1] + double.Parse(data[2]);
+                    products[data[0]][0] = double.Parse(data[1]);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            foreach (var item in products)
+            {
+                Console.WriteLine($"{item.Key} -> {(item.Value[0] * item.Value[1]):f2}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Exercise 7 - Associative Arrays/05.SoftUniParking/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.SoftUniParking
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            Dictionary<string, string> users = new Dictionary<string, string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(' ',StringSplitOptions.RemoveEmptyEntries)
+                    .ToArray();
+
+                string command = input[0];
+                string username = input[1];
+
+                switch (command)
+                {
+                    case "register":
+                        string licensePlate = input[2];
+
+                        if(users.ContainsKey(username))
+                        {
+                            Console.WriteLine($"ERROR: already registered with plate number {users[username]}");
+                        }
+                        else
+                        {
+                            users.Add(username, licensePlate);
+                            Console.WriteLine($"{username} registered {licensePlate} successfully");
+                        }
+                        break;
+                    case "unregister":
+                        if(!users.ContainsKey(username))
+                        {
+                            Console.WriteLine($"ERROR: user {username} not found");
+                        }
+                        else
+                        {
+                            users.Remove(username);
+                            Console.WriteLine($"{username} unregistered successfully");
+                        }
+                        break;
+                }
+            }
+            foreach (var item in users)
+            {
+                Console.WriteLine($"{item.Key} => {item.Value}");
+            }
+        }
+    }
+}
