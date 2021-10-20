5d74f4fdf64222636749a38d495c2ab7202f7d69
HNochev <bgaming@abv.bg>
2021-10-20 20:43:21 +0300

    Add Text Processing Lab

---
+++ Programming Fundamentals/Lab 8 - Text Processing/05.Digits,LettersAndOther/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Text;
+
+namespace _05.Digits_LettersAndOther
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string text = Console.ReadLine();
+
+            StringBuilder sbDigits = new StringBuilder();
+            StringBuilder sbLetters = new StringBuilder();
+            StringBuilder sbNonDigOrLetters = new StringBuilder();
+
+            for (int i = 0; i < text.Length; i++)
+            {
+                if(Char.IsDigit(text[i]))
+                {
+                    sbDigits.Append(text[i]);
+                }
+                else if(Char.IsLetter(text[i]))
+                {
+                    sbLetters.Append(text[i]);
+                }
+                else
+                {
+                    sbNonDigOrLetters.Append(text[i]);
+                }
+            }
+            Console.WriteLine(sbDigits);
+            Console.WriteLine(sbLetters);
+            Console.WriteLine(sbNonDigOrLetters);
+        }
+    }
+}
