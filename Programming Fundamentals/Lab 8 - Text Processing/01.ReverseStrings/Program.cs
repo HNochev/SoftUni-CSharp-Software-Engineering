9cff649853bc1940727be039d1ba1927950c4153
HNochev <bgaming@abv.bg>
2021-10-20 20:41:32 +0300

    Add Text Processing Lab

---
+++ Programming Fundamentals/Lab 8 - Text Processing/01.ReverseStrings/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _01.ReverseStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string word = Console.ReadLine();
+            while (word != "end")
+            {
+                Console.WriteLine($"{word} = {string.Join("",word.ToCharArray().Reverse())}");
+
+                word = Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Lab 8 - Text Processing/02.RepeatStrings/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.RepeatStrings
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            foreach (var word in arr)
+            {
+                for (int i = 0; i < word.Length; i++)
+                {
+                    Console.Write(word);
+                }
+            }
+        }
+    }
+}
