0f7dcfbfbfa0bfd89138433c818e2eeebdf468af
HNochev <bgaming@abv.bg>
2021-10-21 20:25:45 +0300

    Add Text Processing Exercise

---
+++ Programming Fundamentals/Exercise 8 - Text Processing/03.ExtractFile/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03.ExtractFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string location = Console.ReadLine();
+
+            int index = location.LastIndexOf('\\');
+            int index2 = location.LastIndexOf('.');
+
+            Console.WriteLine($"File name: {location.Substring(index + 1, index2 - (index + 1))}");
+            Console.WriteLine($"File extension: {location.Substring(index2 + 1)}");
+        }
+    }
+}
---
+++ Programming Fundamentals/Exercise 8 - Text Processing/05.MultiplyBigNumber/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _05.MultiplyBigNumber
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string bigNumber = Console.ReadLine().TrimStart('0'); //9999
+            int num = int.Parse(Console.ReadLine());              //9
+
+            if(num == 0 || bigNumber == "")
+            {
+                Console.WriteLine("0");
+                return;
+            }
+
+            StringBuilder sb = new StringBuilder();
+            int reminder = 0;
+
+            for (int i = bigNumber.Length - 1; i >= 0; i--)
+            {
+                int result = int.Parse(bigNumber[i].ToString()) * num + reminder;
+
+                reminder = result / 10;
+                result = result % 10;
+
+                sb.Append(result);
+            }
+            if(reminder != 0)
+            {
+                sb.Append(reminder);
+            }
+
+            Console.WriteLine(string.Join("", sb.ToString().Reverse()));
+        }
+    }
+}
+
