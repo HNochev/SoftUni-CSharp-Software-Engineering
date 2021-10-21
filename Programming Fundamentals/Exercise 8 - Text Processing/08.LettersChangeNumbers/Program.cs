403b3d2c8311e0748b0afa2d5b514c98f4174e6d
HNochev <bgaming@abv.bg>
2021-10-21 20:29:16 +0300

    Add Text Processing Exercise

---
+++ Programming Fundamentals/Exercise 8 - Text Processing/08.LettersChangeNumbers/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Linq;
+
+namespace _08.LettersChangeNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            double sum = 0;
+
+            foreach (var word in numbers)
+            {
+                string temp = word;
+                temp = temp.Remove(0, 1);
+                temp = temp.Remove(temp.Length - 1);
+                double number = double.Parse(temp);
+
+                if (Char.IsUpper(word[0]))
+                {
+                    number = number / (word[0] - 64);
+                }
+                else if (Char.IsLower(word[0]))
+                {
+                    number = number * (word[0] - 96);
+                }
+
+                if (Char.IsUpper(word[word.Length - 1]))
+                {
+                    number = number - (word[word.Length - 1] - 64);
+                }
+                else if (Char.IsLower(word[word.Length - 1]))
+                {
+                    number = number + (word[word.Length - 1] - 96);
+                }
+
+                sum = sum + number;
+            }
+            Console.WriteLine($"{sum:f2}");
+        }
+    }
+}
