1a91202eee0e2bfda7e4b69112929fcb1c7af455
HNochev <bgaming@abv.bg>
2021-10-22 20:23:49 +0300

    Add Regular Expressions Lab

---
+++ Programming Fundamentals/Lab 9 - Regular Expressions/02.MatchPhoneNumber/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace _02.MatchPhoneNumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string numbers = Console.ReadLine();
+
+            string pattern = @"[+][359]+([- ])[2]\1[0-9]{3}\1[0-9]{4}\b";
+
+            Regex regex = new Regex(pattern);
+
+            var matches = regex.Matches(numbers);
+
+            Console.WriteLine(string.Join(", ", matches));
+        }
+    }
+}
