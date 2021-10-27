3677da51af121029e9fbba5409e0c2ee74ee11d5
HNochev <bgaming@abv.bg>
2021-10-27 21:27:22 +0300

    Add 07.04.2020 Exam Prep

---
+++ Programming Fundamentals/Exam - Preparations/Mid Exam - Preparation/2020.04.07     07 April 2020/01.CounterStrike/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _01.CounterStrike
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            uint energy = uint.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            byte wins = 0;
+
+            while (input != "End of battle")
+            {
+                uint distance = uint.Parse(input);
+                if (energy < distance)
+                {
+                    Console.WriteLine($"Not enough energy! Game ends with {wins} won battles and {energy} energy");
+                    break;
+                }
+                energy = energy - distance;
+                wins++;
+                if (wins % 3 == 0)
+                {
+                    energy = energy + wins;
+                }
+
+                input = Console.ReadLine();
+            }
+            if(input == "End of battle")
+            {
+                Console.WriteLine($"Won battles: {wins}. Energy left: {energy}");
+            }
+        }
+    }
+}
---
+++ Programming Fundamentals/Exam - Preparations/Mid Exam - Preparation/2020.04.07     07 April 2020/02.ShootForTheWin/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.ShootForTheWin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> targets = Console.ReadLine()
+                .Split(' ', StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            string input = Console.ReadLine();
+            int counter = 0;
+            int indexValue = 0;
+
+            while (input != "End")
+            {
+                int index = int.Parse(input);
+                if (index >= 0 && index < targets.Count)
+                {
+                    indexValue = targets[index];
+                    for (int i = 0; i < targets.Count; i++)
+                    {
+                        if (targets[i] == -1)
+                        {
+                            continue;
+                        }
+                        if (targets[index] == -1)
+                        {
+                            break;
+                        }
+
+                        if (targets[i] > indexValue)
+                        {
+                            targets[i] = targets[i] - indexValue;
+                        }
+                        else if (targets[i] <= indexValue)
+                        {
+                            targets[i] = targets[i] + indexValue;
+                        }
+                    }
+                    targets[index] = -1;
+                }
+                input = Console.ReadLine();
+            }
+            counter = targets.Count(x => x == -1);
+
+            Console.Write($"Shot targets: {counter} -> ");
+            Console.Write(string.Join(' ', targets));
+        }
+    }
+}
