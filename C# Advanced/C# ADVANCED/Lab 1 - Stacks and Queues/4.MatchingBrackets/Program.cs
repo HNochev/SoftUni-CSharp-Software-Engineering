bf231a82e13f39e00dc57a059e76ab8ec57f5cae
HNochev <bgaming@abv.bg>
2021-11-06 22:13:24 +0200

    Add Stacks and Queues Lab

---
+++ C# Advanced/C# ADVANCED/Lab 1 - Stacks and Queues/4.MatchingBrackets/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _4.MatchingBrackets
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<string> brackets = new Stack<string>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '(')
+                {
+                    brackets.Push(i.ToString());
+                }
+                if (input[i] == ')')
+                {
+                    int start = int.Parse(brackets.Pop());
+                    string substr = input.Substring(start, i - start + 1);
+                    Console.WriteLine(substr);
+                }
+            }
+
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Lab 1 - Stacks and Queues/5.PrintEvenNumbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _5.PrintEvenNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] nums = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> even = new Queue<int>(nums.Where(x => x % 2 == 0));
+
+            Console.WriteLine(string.Join(", ", even));
+        }
+    }
+}
