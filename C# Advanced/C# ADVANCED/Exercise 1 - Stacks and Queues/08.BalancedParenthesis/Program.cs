28cee78f803d1b306707fadc68383e8c6c063515
HNochev <bgaming@abv.bg>
2021-11-07 20:05:18 +0200

    Add Stacks and Queues Exercise

---
+++ C# Advanced/C# ADVANCED/Exercise 1 - Stacks and Queues/08.BalancedParenthesis/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _08.BalancedParenthesis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            Stack<char> symbols = new Stack<char>();
+
+            if (input.Length % 2 != 0)
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            if (input[0] == '}' || input[0] == ']' || input[0] == ')')
+            {
+                Console.WriteLine("NO");
+                return;
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '{' || input[i] == '[' || input[i] == '(')
+                {
+                    symbols.Push(input[i]);
+                    continue;
+                }
+                if (input[i] == ')' && symbols.Peek() == '(')
+                {
+                    symbols.Pop();
+                }
+                else if (input[i] == ']' && symbols.Peek() == '[')
+                {
+                    symbols.Pop();
+                }
+                else if (input[i] == '}' && symbols.Peek() == '{')
+                {
+                    symbols.Pop();
+                }
+            }
+            if (symbols.Any())
+            {
+                Console.WriteLine("NO");
+            }
+            else
+            {
+                Console.WriteLine("YES");
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Exercise 1 - Stacks and Queues/09.SimpleTextEditor/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace _09.SimpleTextEditor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            StringBuilder sb = new StringBuilder();
+            Stack<string> texts = new Stack<string>();
+            texts.Push(sb.ToString());
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] command = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                switch (command[0])
+                {
+                    case "1":
+                        {
+                            sb.Append(command[1]);
+                            texts.Push(sb.ToString());
+                            break;
+                        }
+                    case "2":
+                        {
+                            int count = int.Parse(command[1]);
+                            sb.Remove(sb.Length - count, count);
+                            texts.Push(sb.ToString());
+                            break;
+                        }
+                    case "3":
+                        {
+                            int index = int.Parse(command[1]) - 1;
+                            Console.WriteLine(sb[index]);
+                            break;
+                        }
+                    case "4":
+                        {
+                            texts.Pop();
+                            sb.Clear();
+                            sb.Append(texts.Peek());
+                            break;
+                        }
+                    default:
+                        break;
+                }
+            }
+        }
+    }
+}
