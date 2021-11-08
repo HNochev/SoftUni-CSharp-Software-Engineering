56873c8d466ef51a4598bd7e13e232c403cccc9f
HNochev <bgaming@abv.bg>
2021-11-08 21:55:03 +0200

    Add Multidimensional Arrays Lab

---
+++ C# Advanced/C# ADVANCED/Lab 2 - Multidimensional Arrays/08.SpiralMatrix/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace _08.SpiralMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n, n];
+
+            string direction = "right";
+
+            int row = 0;
+            int col = 0;
+
+            for (int i = 0; i < n*n; i++)
+            {
+                matrix[row, col] = i + 1;
+                if(direction == "right")
+                {
+                    col++;
+                }
+                if (direction == "left")
+                {
+                    col--;
+                }
+                if (direction == "down")
+                {
+                    row++;
+                }
+                if (direction == "up")
+                {
+                    row--;
+                }
+
+                if((col == n || matrix[row,col] != 0) && direction == "right")
+                {
+                    col--;
+                    row++;
+                    direction = "down";
+                }
+                if ((row == n || matrix[row, col] != 0) && direction == "down")
+                {
+                    row--;
+                    col--;
+                    direction = "left";
+                }
+                if ((col == -1 || matrix[row, col] != 0) && direction == "left")
+                {
+                    col++;
+                    row--;
+                    direction = "up";
+                }
+                if ((row == -1 || matrix[row, col] != 0) && direction == "up")
+                {
+                    row++;
+                    col++;
+                    direction = "right";
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Lab 2 - Multidimensional Arrays/7.PascalTriangle/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+
+namespace _7.PascalTriangle
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int rows = int.Parse(Console.ReadLine());
+
+            long[][] pascal = new long[rows][];
+
+            for (int row = 0; row < rows; row++)
+            {
+                pascal[row] = new long[row + 1];
+                for (int col = 0; col < row +1; col++)
+                {
+                    if (row - 1 >= 0 && col >= 1 && col < pascal[row -1].Length)
+                    {
+                        pascal[row][col] = pascal[row][col] + pascal[row - 1][col];
+                    }
+                    if (row - 1 >= 0 && col - 1 >= 0)
+                    {
+                        pascal[row][col] = pascal[row][col] + pascal[row - 1][col - 1];
+                    }
+                    if (pascal[row][col] == 0)
+                    {
+                        pascal[row][col] = 1;
+                    }
+                }
+            }
+
+            for (int i = 0; i < pascal.Length; i++)
+            {
+                for (int j = 0; j < pascal[i].Length; j++)
+                {
+                    Console.Write($"{pascal[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
