c142e481bf416dcb80698f4de2d44ac5e52218ba
HNochev <bgaming@abv.bg>
2021-11-08 21:52:13 +0200

    Add Multidimensional Arrays Lab

---
+++ C# Advanced/C# ADVANCED/Lab 2 - Multidimensional Arrays/1.SumMatrixElements/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.SumMatrixElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsAndCols = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = rowsAndCols[0];
+            int cols = rowsAndCols[1];
+            int sum = 0;
+
+            int[,] matrix = new int[rows, cols];
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] numbers = Console.ReadLine()
+                    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = numbers[col];
+                    sum = sum + numbers[col];
+                }
+            }
+            Console.WriteLine(rows);
+            Console.WriteLine(cols);
+            Console.WriteLine(sum);
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Lab 2 - Multidimensional Arrays/3.PrimaryDiagonal/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq;
+
+namespace _3.PrimaryDiagonal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+            int sum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                sum = sum + matrix[i, i];
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
