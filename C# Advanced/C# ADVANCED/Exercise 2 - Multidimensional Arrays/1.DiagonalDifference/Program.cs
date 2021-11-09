84bcb0e1e7a88c5a3fc658a8d0268fdcbb1ca72c
HNochev <bgaming@abv.bg>
2021-11-09 20:18:42 +0200

    Add Multidimensional Arrays Exercise

---
+++ C# Advanced/C# ADVANCED/Exercise 2 - Multidimensional Arrays/1.DiagonalDifference/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace _1.DiagonalDifference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[n, n];
+
+            int primarySum = 0;
+            int secondarySum = 0;
+
+            for (int row = 0; row < n; row++)
+            {
+                int[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < n; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+
+            for (int row = 0; row < n; row++)
+            {
+                for (int col = 0; col < n; col++)
+                {
+                    if (row == col)
+                    {
+                        primarySum = primarySum + matrix[row, col];
+                    }
+                    if (col == n - 1 - row)
+                    {
+                        secondarySum = secondarySum + matrix[row, col];
+                    }
+                }
+            }
+            Console.WriteLine(Math.Abs(primarySum - secondarySum));
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Exercise 2 - Multidimensional Arrays/2.SquaresInMatrix/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+
+namespace _2.SquaresInMatrix
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowsCols = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = rowsCols[0];
+            int cols = rowsCols[1];
+
+            char[,] matrix = new char[rows, cols];
+
+            int countEqualSquares = 0;
+
+            for (int row = 0; row < rows; row++)
+            {
+                char[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(char.Parse)
+                    .ToArray();
+
+                for (int col = 0; col < cols; col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+
+            for (int row = 0; row < rows - 1; row++)
+            {
+                for (int col = 0; col < cols - 1; col++)
+                {
+                    if (matrix[row, col] == matrix[row, col + 1]
+                        && matrix[row, col] == matrix[row + 1, col]
+                         && matrix[row, col] == matrix[row + 1, col + 1])
+                    {
+                        countEqualSquares++;
+                    }
+                }
+            }
+            Console.WriteLine(countEqualSquares);
+        }
+    }
+}
