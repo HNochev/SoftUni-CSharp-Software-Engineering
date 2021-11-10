116bad8f912e6ece88f84d943856bb254d838f91
HNochev <bgaming@abv.bg>
2021-11-10 20:12:52 +0200

    Add Sets and Dictionaries Lab

---
+++ C# Advanced/C# ADVANCED/Lab 3 - Sets and Dictionaries Advanced/06.ParkingLot/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _06.ParkingLot
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            HashSet<string> licensePlates = new HashSet<string>();
+
+            while (input[0] != "END")
+            {
+                if (input[0] == "IN")
+                {
+                    licensePlates.Add(input[1]);
+                }
+                else if (input[0] == "OUT")
+                {
+                    licensePlates.Remove(input[1]);
+                }
+
+                input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+            }
+
+            if (licensePlates.Count > 0)
+            {
+                foreach (var plate in licensePlates)
+                {
+                    Console.WriteLine(plate);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+        }
+    }
+}
