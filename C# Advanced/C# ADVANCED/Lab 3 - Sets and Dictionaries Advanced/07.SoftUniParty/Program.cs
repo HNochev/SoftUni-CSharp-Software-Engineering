5f1948b0a0022511704c6c1c0b3658a947e10317
HNochev <bgaming@abv.bg>
2021-11-10 20:13:58 +0200

    Add Sets and Dictionaries Lab

---
+++ C# Advanced/C# ADVANCED/Lab 3 - Sets and Dictionaries Advanced/07.SoftUniParty/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _07.SoftUniParty
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            HashSet<string> vip = new HashSet<string>();
+            HashSet<string> regular = new HashSet<string>();
+
+            while (input != "PARTY")
+            {
+                if (Char.IsDigit(input[0]))
+                {
+                    vip.Add(input);
+                }
+                else
+                {
+                    regular.Add(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            input = Console.ReadLine();
+            while (input != "END")
+            {
+                if (Char.IsDigit(input[0]))
+                {
+                    vip.Remove(input);
+                }
+                else
+                {
+                    regular.Remove(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine(vip.Count + regular.Count);
+            foreach (var guest in vip)
+            {
+                Console.WriteLine(guest);
+            }
+            foreach (var guest in regular)
+            {
+                Console.WriteLine(guest);
+            }
+        }
+    }
+}
