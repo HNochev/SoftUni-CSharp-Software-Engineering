b59ff0041d33c54565c740e491ff44f4e6e3d4da
HNochev <bgaming@abv.bg>
2021-11-11 23:11:24 +0200

    Add Sets and Dictionaries Exercise

---
+++ C# Advanced/C# ADVANCED/Exercise 3 - Sets and Dictionaries Advanced/02.SetsOfElements/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02.SetsOfElements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] capacities = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            HashSet<int> one = new HashSet<int>();
+            HashSet<int> two = new HashSet<int>();
+
+            int n = capacities[0];
+            int m = capacities[1];
+
+            for (int i = 0; i < n; i++)
+            {
+                one.Add(int.Parse(Console.ReadLine()));
+            }
+            for (int i = 0; i < m; i++)
+            {
+                two.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach (var num in one)
+            {
+                if (two.Contains(num))
+                {
+                    Console.Write($"{num} ");
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Exercise 3 - Sets and Dictionaries Advanced/03.PeriodicTable/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.PeriodicTable
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            HashSet<string> chemicals = new HashSet<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (var item in input)
+                {
+                    chemicals.Add(item);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", chemicals.OrderBy(x => x)));
+        }
+    }
+}
