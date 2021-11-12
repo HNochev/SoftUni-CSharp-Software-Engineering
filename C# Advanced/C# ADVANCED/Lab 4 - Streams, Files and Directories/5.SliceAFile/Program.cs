e91540217c2288c801223e8f5cb21349928f8429
HNochev <bgaming@abv.bg>
2021-11-12 21:53:14 +0200

    Add Streams, Files and Directories Lab

---
+++ C# Advanced/C# ADVANCED/Lab 4 - Streams, Files and Directories/5.SliceAFile/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _5.SliceAFile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using (FileStream streamReader = new FileStream("../../../sliceMe.txt", FileMode.Open))
+            {
+                int chunkSize = (int)Math.Ceiling(streamReader.Length / 4.0);
+
+                for (int i = 0; i < 4; i++)
+                {
+                    byte[] buffer = new byte[1];
+                    int count = 0;
+                    using (FileStream streamWriter = new FileStream($"../../../slice-{i + 1}.txt", FileMode.Create, FileAccess.Write))
+                    {
+                        while (count < chunkSize)
+                        {
+                            streamReader.Read(buffer, 0, buffer.Length);
+                            streamReader.Write(buffer, 0, buffer.Length);
+                            count += buffer.Length;
+                        }
+                        if (streamReader.Position != streamReader.Length && i == 3)
+                        {
+                            int reaminingBytes = (int)streamReader.Length - (int)streamReader.Position;
+                            byte[] lastBuffer = new byte[reaminingBytes];
+                            streamReader.Read(lastBuffer, 0, reaminingBytes);
+                            streamWriter.Write(lastBuffer, 0, lastBuffer.Length);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Lab 4 - Streams, Files and Directories/6.FolderSize/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.IO;
+
+namespace _6.FolderSize
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = $"../../../TestFolder";
+
+            var files = Directory.GetFiles(folderPath);
+
+            double fileSizes = 0;
+
+            foreach (var filePath in files)
+            {
+                FileInfo file = new FileInfo(filePath);
+
+                Console.WriteLine(file.FullName);
+                Console.WriteLine(file.Length);
+                Console.WriteLine(file.Extension);
+                Console.WriteLine(file.DirectoryName);
+
+                fileSizes += file.Length;
+            }
+            Console.WriteLine(fileSizes / 1024 / 1024);
+        }
+    }
+}
