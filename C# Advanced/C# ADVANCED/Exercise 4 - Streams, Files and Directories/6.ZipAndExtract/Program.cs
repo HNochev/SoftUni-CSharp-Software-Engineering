4db8f3c7768a46ef4d7192db7b8455752fb89703
HNochev <bgaming@abv.bg>
2021-11-13 20:25:59 +0200

    Add Streams, Files and Directories Exercise

---
+++ C# Advanced/C# ADVANCED/Exercise 4 - Streams, Files and Directories/6.ZipAndExtract/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace _6.ZipAndExtract
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using ZipArchive zipFile = ZipFile.Open("../../../zipfile.zip", ZipArchiveMode.Create);
+            ZipArchiveEntry zipArchiveEntry = zipFile.CreateEntryFromFile("../../../copyMe.png", "result.png");
+        }
+    }
+}
