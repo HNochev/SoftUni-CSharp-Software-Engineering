7df958ad436814a4dd02f88942e4419787ee9d41
HNochev <bgaming@abv.bg>
2021-11-14 23:32:55 +0200

    Add Functional Programming Lab

---
+++ C# Advanced/C# ADVANCED/Lab 5 - Functional Programming/02.SumNumbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.SumNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            PrintSumAndCount(
+                int.Parse,
+                a => a.Length,
+                array => array.Sum());
+           
+        }
+        static void PrintSumAndCount(Func<string, int> parser
+            , Func<int[], int> countGetter
+            , Func<int[], int> sumCalculator)
+        {
+            int[] array = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(parser)
+                .ToArray();
+
+            Console.WriteLine(countGetter(array));
+            Console.WriteLine(sumCalculator(array));
+        }
+    }
+}
