e64f7237d04c0a07d5bfd824e76be0d0550097f7
HNochev <bgaming@abv.bg>
2021-11-15 20:02:29 +0200

    Add Functional Programming Exercise

---
+++ C# Advanced/C# ADVANCED/Exercise 5 - Functional Programming/04.FindEvensOrOdds/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _04.FindEvensOrOdds
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] startEnd = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int start = startEnd[0];
+            int end = startEnd[1];
+
+            List<int> numbers = new List<int>();
+
+            for (int i = start; i <= end; i++)
+            {
+                numbers.Add(i);
+            }
+
+            string oddEven = Console.ReadLine();
+
+            Console.WriteLine(string.Join(" ", numbers.Where(getOddEven(oddEven))));
+        }
+
+        static Func<int, bool> getOddEven(string oddEven)
+        {
+            switch (oddEven)
+            {
+                case "odd":
+                    {
+                        return x => x % 2 != 0;
+                    }
+                case "even":
+                    {
+                        return x => x % 2 == 0;
+                    }
+                default:
+                    return null;
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Exercise 5 - Functional Programming/05.AppliedArithmetics/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _05.AppliedArithmetics
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            string command = Console.ReadLine();
+
+            Action<List<int>> print = x => Console.WriteLine(string.Join(" ", x));
+
+            while (command != "end")
+            {
+                if (command == "print")
+                {
+                    print(numbers);
+                }
+                else
+                {
+                    Func<int, int> func = GetFunc(command);
+                    numbers = numbers.Select(func).ToList();
+                }
+
+                command = Console.ReadLine();
+            }
+        }
+
+        static Func<int, int> GetFunc(string command)
+        {
+            switch (command)
+            {
+                case "add":
+                    {
+                        return n => n = n + 1;
+                    }
+                case "multiply":
+                    {
+                        return n => n = n * 2;
+                    }
+                case "subtract":
+                    {
+                        return n => n = n - 1;
+                    }
+                default:
+                    return null;
+            }
+        }
+    }
+}
