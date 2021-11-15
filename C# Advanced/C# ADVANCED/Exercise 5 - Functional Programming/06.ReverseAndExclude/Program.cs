4cb75e1546de9d63e628f8c82c263e6536e06478
HNochev <bgaming@abv.bg>
2021-11-15 20:03:45 +0200

    Add Functional Programming Exercise

---
+++ C# Advanced/C# ADVANCED/Exercise 5 - Functional Programming/06.ReverseAndExclude/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _06.ReverseAndExclude
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int n = int.Parse(Console.ReadLine());
+
+
+            numbers.Reverse();
+
+            Func<int, int, bool> filter = (num, divider) => num % divider != 0;
+
+            numbers = numbers.Where(number => filter(number, n)).ToList();
+
+            Action<List<int>> print = x => Console.WriteLine(string.Join(" ", x));
+            print(numbers);
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Exercise 5 - Functional Programming/07.PredicateForNames/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Linq;
+
+namespace _07.PredicateForNames
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            Func<string, int, bool> nameFilter = (name, lenght) => name.Length <= lenght;
+
+            Console.WriteLine(string.Join(Environment.NewLine, names.Where(name => nameFilter(name, n))));
+        }
+    }
+}
