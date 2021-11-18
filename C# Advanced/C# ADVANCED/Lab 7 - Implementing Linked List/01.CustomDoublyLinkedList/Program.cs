507e52ed67a89a5c33464ae271859523e346a3bf
HNochev <bgaming@abv.bg>
2021-11-18 20:04:54 +0200

    Add Implementing Linked List

---
+++ C# Advanced/C# ADVANCED/Lab 7 - Implementing Linked List/01.CustomDoublyLinkedList/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace _01.CustomDoublyLinkedList
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            DoublyLinkedList doublyLinkedList = new DoublyLinkedList();
+
+            doublyLinkedList.AddFirst(1);
+            doublyLinkedList.AddFirst(2);
+            doublyLinkedList.AddFirst(3);
+            doublyLinkedList.AddLast(4);
+            doublyLinkedList.AddLast(5);
+            doublyLinkedList.AddLast(6);
+
+            doublyLinkedList.ForEach(x => Console.WriteLine(x));
+
+            doublyLinkedList.RemoveFirst();
+            doublyLinkedList.RemoveFirst();
+
+            Console.WriteLine();
+            doublyLinkedList.ForEach(x => Console.WriteLine(x));
+
+            doublyLinkedList.RemoveLast();
+
+            Console.WriteLine();
+            doublyLinkedList.ForEach(x => Console.WriteLine(x));
+
+            int[] array = doublyLinkedList.ToArray();
+            Console.WriteLine(string.Join(" ", array));
+        }
+    }
+}
