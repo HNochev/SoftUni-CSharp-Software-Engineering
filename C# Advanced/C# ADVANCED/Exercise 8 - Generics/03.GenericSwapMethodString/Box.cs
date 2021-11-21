ed4c2ba102c66fa7972cec5df5ff9b5e997ab775
HNochev <bgaming@abv.bg>
2021-11-21 18:53:42 +0200

    Add Generics Exercise

---
+++ C# Advanced/C# ADVANCED/Exercise 8 - Generics/03.GenericSwapMethodString/Box.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Box<T>
+    {
+        public Box(T value)
+        {
+            this.Value = value;
+        }
+
+        public T Value { get; set; }
+
+        public override string ToString()
+        {
+            Type type = this.Value.GetType();
+            string fullnameType = type.FullName;
+
+            return $"{fullnameType}: {this.Value}";
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Exercise 8 - Generics/03.GenericSwapMethodString/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _03.GenericSwapMethodString
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            List<Box<string>> boxes = new List<Box<string>>();
+            for (int i = 0; i < n; i++)
+            {
+                Box<string> box = new Box<string>(Console.ReadLine());
+
+                boxes.Add(box);
+            }
+
+            int[] indexes = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Swap(boxes, indexes[0], indexes[1]);
+
+            foreach (Box<string> box in boxes)
+            {
+                Console.WriteLine(box);
+            }
+        }
+        public static void Swap<T>(List<Box<T>> list, int firstIndex, int secondIndex)
+        {
+            Box<T> tempOne = new Box<T>(list[firstIndex].Value);
+            list[firstIndex] = list[secondIndex];
+            list[secondIndex] = tempOne;
+        }
+    }
+}
