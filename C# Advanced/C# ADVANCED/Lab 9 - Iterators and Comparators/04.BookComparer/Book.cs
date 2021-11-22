fa7ea525b5192f61b46f744aa8bb63eac0bd42c7
HNochev <bgaming@abv.bg>
2021-11-22 20:34:05 +0200

    Add Iterators and Comparators Lab

---
+++ C# Advanced/C# ADVANCED/Lab 9 - Iterators and Comparators/04.BookComparer/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+using System.Text;
+
+namespace IteratorsAndComparators
+{
+    public class Book : IComparable<Book>
+    {
+        public Book(string title, int year, params string[] authors)
+        {
+            this.Title = title;
+            this.Year = year;
+            this.Authors = new List<string>(authors);
+        }
+
+        public string Title { get; private set; }
+
+        public int Year { get; private set; }
+
+        public IReadOnlyList<string> Authors { get; private set; }
+
+
+        public int CompareTo(Book other)
+        {
+            if (this.Year.CompareTo(other.Year) != 0)
+            {
+                return this.Year.CompareTo(other.Year);
+            }
+
+            return this.Title.CompareTo(other.Title);
+        }
+
+        public override string ToString()
+        {
+            return $"{this.Title} - {this.Year}";
+        }
+    }
+}
