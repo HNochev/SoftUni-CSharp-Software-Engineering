35ba4b93d7f2cb42129f62aaa8df4ed7ba9419f3
HNochev <bgaming@abv.bg>
2021-11-26 20:14:34 +0200

    Add 25.10.2020 Exam Prep

---
+++ C# Advanced/C# ADVANCED/Exam - Preparation/2020.10.25           25 October 2020/01.Scheduling/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01.Scheduling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Stack<int> tasks = new Stack<int>(Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            Queue<int> threads = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList());
+
+            int taskToBeKilled = int.Parse(Console.ReadLine());
+
+            while (true)
+            {
+                int task = tasks.Pop();
+                int thread = threads.Dequeue();
+
+                if (task == taskToBeKilled)
+                {
+                    Console.WriteLine($"Thread with value {thread} killed task {taskToBeKilled}");
+                    Console.Write($"{thread} ");
+                    break;
+                }
+                if (thread < task)
+                {
+                    tasks.Push(task);
+                }
+            }
+            Console.Write(string.Join(" ", threads));
+
+        }
+    }
+}
---
+++ C# Advanced/C# ADVANCED/Exam - Preparation/2020.10.25           25 October 2020/02.Garden/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Linq;
+
+namespace _02.Garden
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] rowCol = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rows = rowCol[0];
+            int cols = rowCol[1];
+
+            int[,] matrix = new int[rows, cols];
+
+            string command = Console.ReadLine();
+
+            while (command != "Bloom Bloom Plow")
+            {
+                int[] coordinatesRowCol = command
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                    .Select(int.Parse)
+                    .ToArray();
+
+                int row = coordinatesRowCol[0];
+                int col = coordinatesRowCol[1];
+
+                if (row >= rows || col >= cols || row < 0 || col < 0)
+                {
+                    Console.WriteLine("Invalid coordinates.");
+                    command = Console.ReadLine();
+                    continue;
+                }
+
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (i == row)
+                        {
+                            matrix[i, j]++;
+                        }
+                        if (j == col && j != i)
+                        {
+                            matrix[i, j]++;
+                        }
+                    }
+                }
+
+                command = Console.ReadLine();
+            }
+
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
