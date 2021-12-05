3a08f2efbaa6fb8c1b4ca53b37035c69ad320058
HNochev <bgaming@abv.bg>
2021-12-05 21:42:38 +0200

    Add Interfaces and Abstraction Exercise

---
+++ C# Advanced/C# OOP/Exercise 3 - Interfaces and Abstraction/09.ExplicitInterfaces/Contracts/IPerson.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _09.ExplicitInterfaces.Contracts
+{
+    public interface IPerson
+    {
+        public string Name { get; }
+
+        public int Age { get; }
+
+        string GetName();
+    }
+}
---
+++ C# Advanced/C# OOP/Exercise 3 - Interfaces and Abstraction/09.ExplicitInterfaces/Program.cs
@@ -0,0 +1,33 @@
+﻿using _09.ExplicitInterfaces.Contracts;
+using _09.ExplicitInterfaces.Models;
+using System;
+
+namespace _09.ExplicitInterfaces
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string[] input = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (input[0] == "End")
+                {
+                    break;
+                }
+
+                string name = input[0];
+                string country = input[1];
+                int age = int.Parse(input[2]);
+
+                IPerson person = new Citizen(name, country, age);
+                IResident resident = new Citizen(name, country, age);
+
+                Console.WriteLine(person.GetName());
+                Console.WriteLine(resident.GetName());
+            }
+        }
+    }
+}
