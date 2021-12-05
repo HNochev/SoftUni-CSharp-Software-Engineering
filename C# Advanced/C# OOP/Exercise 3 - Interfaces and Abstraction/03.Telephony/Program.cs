fd6163a5e17e1ee9ceab79fa71976051c59f7919
HNochev <bgaming@abv.bg>
2021-12-05 21:36:42 +0200

    Add Interfaces and Abstraction Exercise

---
+++ C# Advanced/C# OOP/Exercise 3 - Interfaces and Abstraction/03.Telephony/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Linq;
+
+namespace _03.Telephony
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+            string[] urls = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToArray();
+
+            SmartPhone smartPhone = new SmartPhone();
+            StationaryPhone stationaryPhone = new StationaryPhone();
+
+            foreach (string number in numbers)
+            {
+                try
+                {
+                    string result = number.Length == 10
+                        ? smartPhone.Call(number)
+                        : stationaryPhone.Call(number);
+
+                    Console.WriteLine(result);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+
+            foreach (string url in urls)
+            {
+                try
+                {
+                    string result = smartPhone.Browse(url);
+
+                    Console.WriteLine(result);
+                }
+                catch (InvalidOperationException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/Exercise 3 - Interfaces and Abstraction/03.Telephony/SmartPhone.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace _03.Telephony
+{
+    public class SmartPhone : ICallable, IBrowsable
+    {
+        public string Call(string number)
+        {
+            if (number.Any(x => !char.IsDigit(x)))
+            {
+                throw new InvalidOperationException("Invalid number!");
+            }
+
+            return $"Calling... {number}";
+        }
+
+        public string Browse(string url)
+        {
+            if (url.Any(x => char.IsDigit(x)))
+            {
+                throw new InvalidOperationException("Invalid URL!");
+            }
+
+            return $"Browsing: {url}!";
+        }
+    }
+}
