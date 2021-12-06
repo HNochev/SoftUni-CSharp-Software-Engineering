f94705edb17b00b6455d73e0144dea32ef4b044e
HNochev <bgaming@abv.bg>
2021-12-06 20:12:15 +0200

    Add Polymorphism Lab

---
+++ C# Advanced/C# OOP/Lab 4 - Polymorphism/03.Shapes/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Shapes
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            Shape rectangle = new Rectangle(2, 3);
+
+            Console.WriteLine(rectangle.CalculatePerimeter());
+            Console.WriteLine(rectangle.CalculateArea());
+            Console.WriteLine(rectangle.Draw());
+
+            Console.WriteLine();
+
+            Shape circle = new Circle(4);
+
+            Console.WriteLine($"{circle.CalculatePerimeter():f2}");
+            Console.WriteLine($"{circle.CalculateArea():f2}");
+            Console.WriteLine(circle.Draw());
+        }
+    }
+}
