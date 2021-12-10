92ca77d42b07625e76abd319c4e71dffd6f31c6a
HNochev <bgaming@abv.bg>
2021-12-10 20:24:47 +0200

    Add SOLID Lab

---
+++ C# Advanced/C# OOP/Lab 6 - SOLID/04. ISP - Interface Segregation/P02. Identity-Before/Contracts/IAccountRegistrationSettings.cs
@@ -0,0 +1,13 @@
+﻿namespace P02._Identity_Before.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IAccountRegistrationSettings
+    {
+        bool RequireUniqueEmail { get; }
+
+        int MinRequiredPasswordLength { get; }
+
+        int MaxRequiredPasswordLength { get; }
+    }
+}
---
+++ C# Advanced/C# OOP/Lab 6 - SOLID/05. DIP - Dependancy Inversion/P02. Worker-Before/Worker.cs
@@ -0,0 +1,13 @@
+﻿using P02._Worker_Before.Contracts;
+using System;
+
+namespace P02._Worker_Before
+{
+    public class Worker : IWorker
+    {
+        public void Work()
+        {
+            Console.WriteLine("Works normaly");
+        }
+    }
+}
