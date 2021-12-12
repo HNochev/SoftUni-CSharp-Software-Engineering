087c7f9355241aa65315795402d406f2e3e52295
HNochev <bgaming@abv.bg>
2021-12-12 19:14:02 +0200

    Add Reflection and Attributes Lab

---
+++ C# Advanced/C# OOP/Lab 7 - Reflection and Attributes/05.CreateAttribute/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("UserOne")]
+    public class StartUp
+    {
+        [Author("UserTwo")]
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ C# Advanced/C# OOP/Lab 7 - Reflection and Attributes/06.CodeTracker/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace AuthorProblem
+{
+    [Author("UserOne")]
+    public class StartUp
+    {
+        [Author("UserTwo")]
+        static void Main(string[] args)
+        {
+            var tracker = new Tracker();
+            tracker.PrintMethodsByAuthor();
+        }
+    }
+}
