ee0ad146925e3c79b02baeeb33cf1b6bf5890213
HNochev <bgaming@abv.bg>
2022-01-01 17:41:52 +0200

    Add Advanced Functions Lab

---
+++ Javascript Advanced/JS Advanced/Lab-6-Advanced-Functions/02.Add.js
@@ -0,0 +1,9 @@
+function solution(num) {
+    return function (anotherNum) {
+        return Number(num + anotherNum);
+    };
+}
+
+let add5 = solution(5);
+console.log(add5(2));
+console.log(add5(3));
