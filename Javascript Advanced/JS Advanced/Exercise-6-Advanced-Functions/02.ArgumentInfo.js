4fe8b7f96ff456da413517484f7e2a9abbdcb6d7
HNochev <bgaming@abv.bg>
2022-01-02 14:22:57 +0200

    Add Advanced Functions Exercise

---
+++ Javascript Advanced/JS Advanced/Exercise-6-Advanced-Functions/02.ArgumentInfo.js
@@ -0,0 +1,22 @@
+function solve(...params) {
+    let occurences = {};
+
+    let result = [];
+
+    params.forEach(el => {
+        let type = typeof (el);
+
+        result.push(`${type}: ${el}`);
+        occurences[type] = occurences[type] !== undefined
+            ? ++occurences[type]
+            : 1;
+    });
+
+    Object.keys(occurences)
+        .sort((a, b) => occurences[b] - occurences[a])
+        .forEach(key => result.push(`${key} = ${occurences[key]}`));
+
+    console.log(result.join('\n'));
+}
+
+solve('cat', 42, function () { console.log('Hello world!'); });
