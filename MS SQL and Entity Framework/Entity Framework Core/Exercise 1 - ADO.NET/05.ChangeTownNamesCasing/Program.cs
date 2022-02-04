ad12ae873b12ed3d557ff55acc40a3b4cea5b4b8
HNochev <bgaming@abv.bg>
2022-02-04 21:23:38 +0200

    Add ADO.NET

---
+++ MS SQL and Entity Framework/Entity Framework Core/Exercise 1 - ADO.NET/05.ChangeTownNamesCasing/Program.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+
+namespace _05.ChangeTownNamesCasing
+{
+    class Program
+    {
+        const string SqlConnectionString = "Server=DESKTOP-QE96OKD\\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+        static void Main(string[] args)
+        {
+            string countryName = Console.ReadLine();
+
+            using (var connection = new SqlConnection(SqlConnectionString))
+            {
+                connection.Open();
+
+                var rowsAffected = GetAffectedRows(connection, countryName);
+
+                if(rowsAffected <= 0)
+                {
+                    Console.WriteLine("No town names were affected.");
+                    return;
+                }
+
+                Console.WriteLine($"{rowsAffected} town names were affected.");
+
+                List<string> townNames = GetTownsByName(connection, countryName);
+
+                Console.WriteLine($"[{string.Join(", ", townNames)}]");
+            }
+        }
+
+        private static List<string> GetTownsByName(SqlConnection connection, string countryName)
+        {
+            var selectTowns = @" SELECT t.Name 
+   FROM Towns as t
+   JOIN Countries AS c ON c.Id = t.CountryCode
+  WHERE c.Name = @countryName";
+
+            List<string> towns = new List<string>();
+
+            using (var command = new SqlCommand(selectTowns, connection))
+            {
+                command.Parameters.AddWithValue("@countryName", countryName);
+
+                using (var dataReader = command.ExecuteReader())
+                {
+                    while(dataReader.Read())
+                    {
+                        towns.Add(dataReader[0].ToString());
+                    }
+                }
+            }
+
+            return towns;
+        }
+
+        private static int GetAffectedRows(SqlConnection connection, string countryName)
+        {
+            string updateQuery = @"UPDATE Towns
+   SET Name = UPPER(Name)
+ WHERE CountryCode = (SELECT c.Id FROM Countries AS c WHERE c.Name = @countryName)";
+
+            using (var command = new SqlCommand(updateQuery, connection))
+            {
+                command.Parameters.AddWithValue("@countryName", countryName);
+
+                return command.ExecuteNonQuery();
+            }
+        }
+    }
+}
---
+++ MS SQL and Entity Framework/Entity Framework Core/Exercise 1 - ADO.NET/06.RemoveVillain/Program.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+
+namespace _06.RemoveVillain
+{
+    class Program
+    {
+        const string SqlConnectionString = "Server=DESKTOP-QE96OKD\\SQLEXPRESS;Database=MinionsDB;Integrated Security=true";
+        static void Main(string[] args)
+        {
+            int villainId = int.Parse(Console.ReadLine());
+
+            using(var connection = new SqlConnection(SqlConnectionString))
+            {
+                connection.Open();
+
+                string villainName = GetVillainNameById(connection, villainId);
+
+                if(villainName == null)
+                {
+                    Console.WriteLine("No such villain was found.");
+                    return;
+                }
+
+                var releasedMinions = GetReleasedMinions(connection, villainId);
+
+                DeleteVillainById(connection, villainId);
+
+                Console.WriteLine($"{villainName} was deleted.");
+                Console.WriteLine($"{releasedMinions} minions were released.");
+            }
+        }
+
+        private static void DeleteVillainById(SqlConnection connection, int villainId)
+        {
+            string deleteVillain = @"DELETE FROM Villains
+      WHERE Id = @villainId";
+
+            using (var command = new SqlCommand(deleteVillain, connection))
+            {
+                command.Parameters.AddWithValue("@villainId", villainId);
+
+                command.ExecuteNonQuery();
+            }
+        }
+
+        private static object GetReleasedMinions(SqlConnection connection, int villainId)
+        {
+            string deleteFromMappingTable = @"DELETE FROM MinionsVillains 
+      WHERE VillainId = @villainId";
+
+            using (var command = new SqlCommand(deleteFromMappingTable, connection))
+            {
+                command.Parameters.AddWithValue("@villainId", villainId);
+
+                return command.ExecuteNonQuery();
+            }
+        }
+
+        private static string GetVillainNameById(SqlConnection connection, int villainId)
+        {
+            string selectVillan = @"SELECT Name FROM Villains WHERE Id = @villainId";
+
+            using (var command = new SqlCommand(selectVillan, connection))
+            {
+                command.Parameters.AddWithValue("@villainId", villainId);
+
+                return (string)command.ExecuteScalar();
+            }
+        }
+    }
+}
