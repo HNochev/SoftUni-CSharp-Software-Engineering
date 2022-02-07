dffebe907c329b46175663c1dd64a8e468f502e3
HNochev <bgaming@abv.bg>
2022-02-07 21:00:56 +0200

    Add Entity Relations

---
+++ MS SQL and Entity Framework/Entity Framework Core/Exercise 4 - Entity Relations/P03_FootballBetting/Migrations/20211102173108_editingMistake.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace P03_FootballBetting.Migrations
+{
+    public partial class editingMistake : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bets_Players_PlayerId",
+                table: "Bets");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bets_PlayerId",
+                table: "Bets");
+
+            migrationBuilder.DropColumn(
+                name: "PlayerId",
+                table: "Bets");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "PlayerId",
+                table: "Bets",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bets_PlayerId",
+                table: "Bets",
+                column: "PlayerId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bets_Players_PlayerId",
+                table: "Bets",
+                column: "PlayerId",
+                principalTable: "Players",
+                principalColumn: "PlayerId",
+                onDelete: ReferentialAction.Restrict);
+        }
+    }
+}
