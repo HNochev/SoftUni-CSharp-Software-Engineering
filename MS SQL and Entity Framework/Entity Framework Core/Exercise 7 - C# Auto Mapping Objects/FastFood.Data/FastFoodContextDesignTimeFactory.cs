482944caac0ff180673ada5d87733d77e56ec0a1
HNochev <bgaming@abv.bg>
2022-02-10 20:21:54 +0200

    Add Auto Mapping Objects

---
+++ MS SQL and Entity Framework/Entity Framework Core/Exercise 7 - C# Auto Mapping Objects/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Design;
+
+namespace FastFood.Data
+{
+    public class FastFoodContextDesignTimeFactory : IDesignTimeDbContextFactory<FastFoodContext>
+    {
+        public FastFoodContext CreateDbContext(string[] args)
+        {
+            var bulder = new DbContextOptionsBuilder<FastFoodContext>();
+
+            bulder.
+                UseSqlServer("Server=DESKTOP-QE96OKD\\SQLEXPRESS;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+
+            return new FastFoodContext(bulder.Options);
+        }
+    }
+}
